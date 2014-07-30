bc6b33767ec84a99e1befd3849b4a6798a767498
Jeremy Whitaker <github@jwhitaker.info>
2014-07-29 19:52:04 -0700

    provide proper handling of odd numbered user inputs

--- lib/lucky_number.rb
+++ lib/lucky_number.rb
@@ -1,9 +1,9 @@
 
 def lucky_number(number)
   numbers = number.to_s.split('').map { |each| each.to_i  }
-  front_half = numbers[0..(numbers.length/2).floor-1]
-  back_half = numbers[numbers.length/2.ceil..-1]
-
+  # front_half = numbers.length/2.floor
+  front_half = numbers[0..numbers.length/2 -1]
+  back_half = numbers[(numbers.length/2.0).ceil..-1]
 
   back_half = back_half.inject(:+)
   front_half = front_half.inject(:+)
@@ -12,3 +12,5 @@ def lucky_number(number)
     "You are lucky"
   end
 end
+
+lucky_number(524434)
--- spec/lucky_number_spec.rb
+++ spec/lucky_number_spec.rb
@@ -5,4 +5,8 @@ describe ("lucky_number") do
   it("sums the sides of a number of even length and returns 'You are lucky' if sides are equal") do
     lucky_number(5234).should eq("You are lucky")
   end
+
+  it("can properly account for the middle digit of odd numbered inputs") do
+    lucky_number(52434).should eq("You are lucky")
+  end
 end
