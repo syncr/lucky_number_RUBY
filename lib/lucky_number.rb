c62e0901b5806d7d57286980eae2303eb4260361
Jeremy Whitaker <github@jwhitaker.info>
2014-07-29 20:25:56 -0700

    add additional conditions to test user input and alert user when they are not lucky

--- lib/lucky_number.rb
+++ lib/lucky_number.rb
@@ -1,22 +1,20 @@
 
 def lucky_number(number)
  
-
-
   numbers = number.to_s.split('').map { |each| each.to_i  }
-  # front_half = numbers.length/2.floor
   front_half = numbers[0..numbers.length/2 -1]
   back_half = numbers[(numbers.length/2.0).ceil..-1]
 
-  back_half = back_half.inject(:+)
   front_half = front_half.inject(:+)
+  back_half = back_half.inject(:+)
 
   if number.is_a?(Integer) == false
-   "Please enter a number"
-  elsif
-   front_half == back_half 
+    "Please enter a number"
+  elsif back_half == nil
+    "Please enter a number with more than one digit"
+  elsif front_half == back_half 
     "You are lucky"
+  else "Sorry you are not lucky"  
   end
 end
 
-lucky_number(524434)
--- spec/lucky_number_spec.rb
+++ spec/lucky_number_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require 'lucky_number'
 
 describe ("lucky_number") do
-  it("sums the sides of a number of even length and returns 'You are lucky' if sides are equal") do
+  it("sums the sides of a number of even length and returns 'You are lucky' if the sum of sides is equal") do
     lucky_number(5234).should eq("You are lucky")
   end
 
@@ -14,4 +14,11 @@ describe ("lucky_number") do
     lucky_number("asdf").should eq("Please enter a number")
   end
 
+  it('will ask user to try again when entering a one digit number') do
+    lucky_number(5).should eq("Please enter a number with more than one digit")
+  end
+
+  it('will notify the user when they are not lucky') do
+    lucky_number(1234567).should eq("Sorry you are not lucky")
+  end
 end
