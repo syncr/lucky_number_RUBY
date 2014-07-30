7ca301a50fb0fc8acea190c88dc348e95ade016a
Jeremy Whitaker <github@jwhitaker.info>
2014-07-29 18:51:08 -0700

    provide basic logic to split array and compare the sides

---
+++ README.md
@@ -0,0 +1,9 @@
+Russian Lucky Number
+====================
+
+In some countries of former Soviet Union there was a belief about lucky tickets. A transport ticket of any sort was believed to be lucky if the sum of the digits on the left half of its number was equal to the sum of digits on the right half.
+
+This ruby app takes a user's inputted number and calculates if it is a lucky number.
+
+Author: Jeremy Whitaker
+
--- lib/lucky_number.rb
+++ lib/lucky_number.rb
@@ -1,11 +1,14 @@
 
 def lucky_number(number)
-  numbers=number.to_s.split('')
+  numbers = number.to_s.split('').map { |each| each.to_i  }
+  front_half = numbers[0..(numbers.length/2).floor-1]
+  back_half = numbers[numbers.length/2.ceil..-1]
 
-  if numbers.length%2==0
+
+  back_half = back_half.inject(:+)
+  front_half = front_half.inject(:+)
+
+  if front_half == back_half 
     "You are lucky"
   end
-
 end
-
-lucky_number(5234)
